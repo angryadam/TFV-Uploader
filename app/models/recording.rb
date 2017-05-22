ff755a3ce1743b636674271d10eb3d098ecef9ce
Adam Myers <adam@tyemill.com>
2017-05-22 10:52:53 -0700

    Styles app and cleans up UI.

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -1,4 +1,5 @@
 class Recording < ApplicationRecord
   mount_uploader :recording, RecordingUploader
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :recording, presence: true
 end
